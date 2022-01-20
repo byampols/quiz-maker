c84db4ddf0abfe401c57f063b6f5ab4b01bb638f
unknown <byampols@alumni.cmu.edu>
2022-01-20 09:49:22 -0500

    hopefully fixed render bug

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html')); //client/build/index
+  res.sendFile(path.join(__dirname, '../client/build/index')); //client/public/index
 });
 
 db.once('open', () => {
